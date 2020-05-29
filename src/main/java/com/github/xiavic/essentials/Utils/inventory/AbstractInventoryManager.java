f2efe311d59233507503cf549f7c1b7ada654b87
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-05-29 17:28:00 +0200

    Update database access, add an InventoryManager

---
+++ src/main/java/com/github/xiavic/essentials/Utils/inventory/AbstractInventoryManager.java
@@ -0,0 +1,26 @@
+package com.github.xiavic.essentials.Utils.inventory;
+
+import com.github.xiavic.lib.inventory.InventorySerializer;
+import com.github.xiavic.lib.serialization.DataAccessObject;
+import org.bukkit.inventory.PlayerInventory;
+import org.jetbrains.annotations.NotNull;
+
+public class AbstractInventoryManager {
+
+    @NotNull private final DataAccessObject database;
+    @NotNull private final InventorySerializer serializer;
+
+    public AbstractInventoryManager(@NotNull final DataAccessObject database, @NotNull final InventorySerializer serializer) {
+        this.database = database;
+        this.serializer = serializer;
+    }
+
+    public DataAccessObject getDatabase() {
+        return database;
+    }
+
+    public void saveInventory(@NotNull final String key, @NotNull final PlayerInventory inventory) {
+        byte[] binary = serializer.serialize(inventory);
+        database.save(key, binary);
+    }
+}
