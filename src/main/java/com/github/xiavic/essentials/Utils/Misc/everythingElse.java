a7f29b4d73e3834882c5215550c8b69fdcf961a3
PryPurity <imhandmade1234@gmail.com>
2020-06-27 21:34:37 -0500

    Fuck iron golems and their poppys...

--- src/main/java/com/github/xiavic/essentials/Utils/Misc/everythingElse.java
+++ src/main/java/com/github/xiavic/essentials/Utils/Misc/everythingElse.java
@@ -1,6 +1,14 @@
 package com.github.xiavic.essentials.Utils.Misc;
 
+import com.github.xiavic.essentials.Main;
+import org.bukkit.Material;
+import org.bukkit.entity.IronGolem;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Random;
 
 public enum everythingElse {
 
@@ -8,5 +16,24 @@ public enum everythingElse {
 
     public static boolean isFrozen(Player player) {
         return player.getWalkSpeed() == 0;
+    }
+
+    public void onIGDeath(EntityDeathEvent de) {
+        LivingEntity le = de.getEntity();
+        if (le instanceof IronGolem) {
+            if (!Main.mainConfig.getBoolean("IronGolems.IGRoses")) {
+                de.getDrops().remove(Material.POPPY);
+            }
+            if (!Main.mainConfig.getBoolean("IronGolems.IGIron")) {
+                de.getDrops().remove(Material.IRON_INGOT);
+            }
+            if (Main.mainConfig.getBoolean("IronGolems.CustomIron")) {
+                Random r = new Random();
+                int min = Main.mainConfig.getInt("IronGolems.CustomIron.amountMin");
+                int max = Main.mainConfig.getInt("IronGolems.CustomIron.amountMax");
+                int i = r.nextInt((max - min) + 1) + min;
+                de.getDrops().add(i, new ItemStack(Material.IRON_INGOT));
+            }
+        }
     }
 }
